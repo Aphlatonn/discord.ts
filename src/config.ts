b08b9e3b91aa504b4c578eca397a801a4d17961e
KwaYLen.Marwane_Souiri <marwane.souiri.0@gmail.com>
2024-05-20 18:29:57 +0100

    Adding the config file

---
+++ src/config.ts
@@ -0,0 +1,50 @@
+export default {
+    client: {
+        token: "your application token",
+        id: "your client id",
+    },
+
+    handler: {
+        prefix: ".t ",
+        deploy: true,
+    },
+
+    commands: {
+        slash: true,
+        prefix: true,
+        nonprefix: true,
+    },
+
+    users: {
+        developers: ["144694124925288448"],
+    },
+
+    development: {
+        enabled: false,
+        guild: "",
+    },
+
+    emojis: {
+        0: {
+            success: ``,
+            xmark: ``,
+            alert: ``,
+            attention: ``,
+            gear: ``,
+        },
+    },
+
+    applicationInfo: {
+        supportserver: "https://discord.gg/quantom",
+    },
+};
+
+/**
+ * Project: Template
+ * Author: @Aphlaton
+ * Company: @quantom
+ * Copyright (c) 2024. All rights reserved.
+ * This code is the property of Coder and may not be reproduced or
+ * modified without permission. For more information, contact us at
+ * https://discord.gg/quantom
+ */
