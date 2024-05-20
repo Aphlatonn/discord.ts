f34dc75d53f5e2e04471884257e66bea42cc6b01
KwaYLen.Marwane_Souiri <marwane.souiri.0@gmail.com>
2024-05-20 18:21:00 +0100

    Adding some classes to make creating commands and components better

---
+++ src/classes/Commands.ts
@@ -0,0 +1,110 @@
+import { PermissionsString, SlashCommandBuilder, CommandInteraction, Message } from 'discord.js';
+import Aphlaton from './Aphlaton.js';
+
+type MRunFunction = (client: Aphlaton, message: Message, args: string[]) => Promise<void>;
+type IRunFunction = (client: Aphlaton, interaction: CommandInteraction) => Promise<void>;
+
+
+export class AphlatonMessageCommandBuilder {
+    name: string = '';
+    aliases: string[] = [];
+    cooldown: number = 0;
+    description: string = '';
+    category: string = '';
+    usage: string[] = [];
+    examples: string[] = [];
+    botPerms: PermissionsString[] = [];
+    userPerms: PermissionsString[] = [];
+    nsfw: boolean = false;
+    run: MRunFunction = async () => { };
+
+    setName(name: string): this {
+        this.name = name;
+        return this;
+    }
+
+    setAliases(aliases: string[]): this {
+        this.aliases = aliases;
+        return this;
+    }
+
+    setCooldown(cooldown: number): this {
+        this.cooldown = cooldown;
+        return this;
+    }
+
+    setDescription(description: string): this {
+        this.description = description;
+        return this;
+    }
+
+
+    setCategory(category: string): this {
+        this.category = category;
+        return this;
+    }
+
+    setUsage(usage: string[]): this {
+        this.usage = usage;
+        return this;
+    }
+
+    setExamples(examples: string[]): this {
+        this.examples = examples;
+        return this;
+    }
+
+    setBotPerms(botPerms: PermissionsString[]): this {
+        this.botPerms = botPerms;
+        return this;
+    }
+
+    setUserPerms(userPerms: PermissionsString[]): this {
+        this.userPerms = userPerms;
+        return this;
+    }
+
+    setRun(run: MRunFunction): this {
+        this.run = run;
+        return this;
+    }
+}
+
+export class AphlatonSlashCommandBuilder {
+    command: SlashCommandBuilder
+    cooldown: number = 0;
+    category: string = '';
+    botPerms: PermissionsString[] = [];
+    userPerms: PermissionsString[] = [];
+    run: IRunFunction = async () => { };
+
+    setCommand(command: SlashCommandBuilder): this {
+        this.command = command;
+        return this;
+    }
+
+    setCooldown(cooldown: number): this {
+        this.cooldown = cooldown;
+        return this;
+    }
+
+    setCategory(category: string): this {
+        this.category = category;
+        return this;
+    }
+
+    setBotPerms(botPerms: PermissionsString[]): this {
+        this.botPerms = botPerms;
+        return this;
+    }
+
+    setUserPerms(userPerms: PermissionsString[]): this {
+        this.userPerms = userPerms;
+        return this;
+    }
+
+    setRun(run: IRunFunction): this {
+        this.run = run;
+        return this;
+    }
+}
---
+++ src/classes/Components.ts
@@ -0,0 +1,39 @@
+import { PermissionsString, Interaction } from 'discord.js';
+import Aphlaton from './Aphlaton.js';
+
+type RunFunction = (client: Aphlaton, interaction: Interaction) => Promise<void>;
+
+
+export class AphlatonComponentBuilder {
+    id: string = '';
+    cooldown: number = 0;
+    botPerms: PermissionsString[] = [];
+    userPerms: PermissionsString[] = [];
+    nsfw: boolean = false;
+    run: RunFunction = async () => { };
+
+    setId(id: string): this {
+        this.id = id;
+        return this;
+    }
+
+    setCooldown(cooldown: number): this {
+        this.cooldown = cooldown;
+        return this;
+    }
+
+    setBotPerms(botPerms: PermissionsString[]): this {
+        this.botPerms = botPerms;
+        return this;
+    }
+
+    setUserPerms(userPerms: PermissionsString[]): this {
+        this.userPerms = userPerms;
+        return this;
+    }
+
+    setRun(run: RunFunction): this {
+        this.run = run;
+        return this;
+    }
+}
