ac0fe0e5601bcbb2c2c6c3d16470df51904abe1c
KwaYLen.Marwane_Souiri <marwane.souiri.0@gmail.com>
2024-05-22 13:43:02 +0100

    Adding the context menu builder

---
+++ src/classes/ContextMenus.ts
@@ -0,0 +1,44 @@
+import { PermissionsString, CommandInteraction, ContextMenuCommandBuilder } from 'discord.js';
+import Aphlaton from './Aphlaton.js';
+
+type RunFunction = (client: Aphlaton, interaction: CommandInteraction) => Promise<void>;
+
+
+export class AphlatonContextMenuBuilder {
+    command: ContextMenuCommandBuilder
+    cooldown: number = 0;
+    botPerms: PermissionsString[] = [];
+    userPerms: PermissionsString[] = [];
+    nsfw: boolean = false;
+    run: RunFunction = async () => { };
+
+    setCommand(command: ContextMenuCommandBuilder): this {
+        this.command = command;
+        return this;
+    }
+
+    setCooldown(cooldown: number): this {
+        this.cooldown = cooldown;
+        return this;
+    }
+
+    setBotPerms(botPerms: PermissionsString[]): this {
+        this.botPerms = botPerms;
+        return this;
+    }
+
+    setUserPerms(userPerms: PermissionsString[]): this {
+        this.userPerms = userPerms;
+        return this;
+    }
+
+    setNSFW(bool: boolean): this {
+        this.nsfw = bool;
+        return this;
+    }
+
+    setRun(run: RunFunction): this {
+        this.run = run;
+        return this;
+    }
+}
