4524f71ce4d920a8f86acdb3bedb4e22b983c34b
KwaYLen.Marwane_Souiri <marwane.souiri.0@gmail.com>
2024-05-22 20:08:11 +0100

    fixing the app crash in unhandled promises rejections

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
 import Aphlaton from "./classes/Aphlaton.js";
+import process from "process";
 import * as dotenv from "dotenv";
+import config from "./config.js";
+import { log } from "./functions.js";
 
 dotenv.config();
 
+// create the aphlaton client instance
 const client = new Aphlaton();
-client.start(process.env.CLIENT_TOKEN);
+client.start(process.env.CLIENT_TOKEN || config.client.token);
+
+// Register a handler for unhandled promise rejections
+process.on("unhandledRejection", (reason) => {
+    log("Unhandled Rejection: ", "err");
+    console.error(reason);
+});
+
+/**
+ * Project: Template
+ * Author: @Aphlaton
+ * this code is under the MIT license.
+ * For more information, contact us at
+ * https://discord.gg/quantom
+ */
+
