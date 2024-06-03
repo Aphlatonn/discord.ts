8c29a9f4b7f978d04b93bc8496957b4b94d2d7c8
KwaYLen.Marwane_Souiri <marwane.souiri.0@gmail.com>
2024-06-03 17:52:26 +0100

    Adding the developers permission check to the event

--- src/events/aphlaton/components.ts
+++ src/events/aphlaton/components.ts
@@ -3,6 +3,7 @@ import Aphlaton from "../../classes/Aphlaton.js";
 import { AphlatonEventBuilder } from "../../classes/events.js";
 import { log } from "../../functions.js";
 import { AphlatonComponentBuilder } from "../../classes/Components.js";
+import config from "../../config.js";
 
 export default new AphlatonEventBuilder()
     .setEvent('interactionCreate')
@@ -59,9 +60,9 @@ export default new AphlatonEventBuilder()
         for (const per of component.data.botPerms) {
             if (!interaction.guild.members.me.permissions.has(per)) {
                 if (interaction.isRepliable()) {
-                    interaction.reply(`i need the \`${per}\` permission to execute this command.`);
+                    interaction.reply(`i need the \`${per}\` permission to execute this component.`);
                 } else {
-                    interaction.channel.send(`i need the \`${per}\` permission to execute this command.`);
+                    interaction.channel.send(`i need the \`${per}\` permission to execute this component.`);
                 }
                 return
             };
@@ -71,9 +72,9 @@ export default new AphlatonEventBuilder()
         for (const per of component.data.userPerms) {
             if (!(interaction.member.permissions as PermissionsBitField).has(per)) {
                 if (interaction.isRepliable()) {
-                    interaction.reply(`you need the \`${per}\` permission to use this command.`);
+                    interaction.reply(`you need the \`${per}\` permission to use this component.`);
                 } else {
-                    interaction.channel.send(`you need the \`${per}\` permission to use this command.`);
+                    interaction.channel.send(`you need the \`${per}\` permission to use this component.`);
                 }
                 return
             };
@@ -82,9 +83,19 @@ export default new AphlatonEventBuilder()
         // check the nsfw
         if (interaction.channel instanceof TextChannel || interaction.channel instanceof VoiceChannel && component.data.nsfw && !interaction.channel.nsfw) {
             if (interaction.isRepliable()) {
-                interaction.reply('this command can only be used in nsfw channels.');
+                interaction.reply('this component can only be used in nsfw channels.');
             } else {
-                interaction.channel.send('this command can only be used in nsfw channels.');
+                interaction.channel.send('this component can only be used in nsfw channels.');
+            }
+            return
+        }
+
+        // check the developers only perms
+        if (component.data.developersOnly && !config.users.developers.includes(interaction.user.id)) {
+            if (interaction.isRepliable()) {
+                interaction.reply('only developers can use this component.');
+            } else {
+                interaction.channel.send('only developers can use this component.');
             }
             return
         }
